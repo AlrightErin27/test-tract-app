08b1a9d458909412631bbce5b4346b48d59ab82c
Erin Van Brunt <77031790+AlrightErin27@users.noreply.github.com>
2024-06-05 18:25:39 -0700

    game is working on basic level

--- src/pages/TicTacToe/TicTacToe.js
+++ src/pages/TicTacToe/TicTacToe.js
@@ -18,25 +18,62 @@ export default function TicTacToe() {
     checkWin();
     checkTie();
     player == "X" ? setPlayer("O") : setPlayer("X");
-  }, [board]);
+  }, [board]); //this fxn runs after everytime setBoard is used
+
+  useEffect(() => {
+    if (result.gameState != "in-play") alert(`FIN! Winner: ${result.winner}`);
+  }, [result]); //fxn runs when result has changed to !"in-play"
 
   //CHOOSE SQUARE
   const selectSquare = (square) => {
-    //update square to current player
     setBoard(
+      //map through board arr & get it's value and it's index
+      //update square to current player
       board.map((val, idx) => {
+        //example) if someone clicks square1 it goes to the board arr index of 1 & IF its currently empty ("")->
+        //then it the current players char is returned as the square's value (changing it from "" to either "X" or "O", which is displayed in Square.js)
         if (idx == square && val == "") {
           return player;
         }
+        //if the value isn't changed the preset value "" is stilll displayed
         return val;
       })
     );
   };
 
   //CHECK IF WINNING PATTERN EXISTS
-  const checkWin = () => {};
+  const checkWin = () => {
+    //loop through each pattern/arr
+    WinningPatterns.forEach((currPattern) => {
+      const playingPlayer = board[currPattern[0]];
+      if (playingPlayer == "") return;
+
+      let foundWinningPattern = true;
+
+      currPattern.forEach((idx) => {
+        if (board[idx] != playingPlayer) {
+          foundWinningPattern = false;
+        }
+      });
+
+      if (foundWinningPattern) {
+        setResult({ winner: player, gameState: "won" });
+      }
+    });
+  };
   //CHECK IF TIE
-  const checkTie = () => {};
+  const checkTie = () => {
+    let filled = true;
+
+    board.forEach((square) => {
+      if (square == "") {
+        filled = false;
+      }
+    });
+    if (filled) {
+      setResult({ winner: "no one", gameState: "cats" });
+    }
+  };
 
   return (
     <div>
