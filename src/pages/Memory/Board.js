70dc85be950ee9ae23837045e13c6f0f031d6144
Erin Van Brunt <77031790+AlrightErin27@users.noreply.github.com>
2024-06-19 18:06:15 -0700

    very basic random rendering of face down cards

--- src/pages/Memory/Board.js
+++ src/pages/Memory/Board.js
@@ -1,5 +1,36 @@
 import "./Memory.css";
+import Card from "./Card";
+// import { useState } from "react";
 
 export default function Board() {
-  return <div className="Board">Board</div>;
+  const stylesArr = [
+    ["♥", "#993300"],
+    ["♣️", "#00004d"],
+    ["♠", "#e5f9f8"],
+    ["♡", "#0066ff"],
+    ["♢", "#004d00"],
+    ["♧", "#666600"],
+  ];
+  const idx = Math.floor(Math.random() * stylesArr.length);
+
+  return (
+    <div className="Board">
+      <div className="row">
+        <Card emoji={stylesArr[idx][0]} cardColor={stylesArr[idx][1]} />
+        <Card emoji={stylesArr[idx][0]} cardColor={stylesArr[idx][1]} />
+        <Card emoji={stylesArr[idx][0]} cardColor={stylesArr[idx][1]} />
+        <Card emoji={stylesArr[idx][0]} cardColor={stylesArr[idx][1]} />
+        <Card emoji={stylesArr[idx][0]} cardColor={stylesArr[idx][1]} />
+        <Card emoji={stylesArr[idx][0]} cardColor={stylesArr[idx][1]} />
+      </div>
+      <div className="row">
+        <Card emoji={stylesArr[idx][0]} cardColor={stylesArr[idx][1]} />
+        <Card emoji={stylesArr[idx][0]} cardColor={stylesArr[idx][1]} />
+        <Card emoji={stylesArr[idx][0]} cardColor={stylesArr[idx][1]} />
+        <Card emoji={stylesArr[idx][0]} cardColor={stylesArr[idx][1]} />
+        <Card emoji={stylesArr[idx][0]} cardColor={stylesArr[idx][1]} />
+        <Card emoji={stylesArr[idx][0]} cardColor={stylesArr[idx][1]} />
+      </div>
+    </div>
+  );
 }
--- src/pages/Memory/Card.js
+++ src/pages/Memory/Card.js
@@ -1,5 +1,18 @@
 import "./Memory.css";
+import { useState, useEffect } from "react";
 
-export default function Card() {
-  return <h1>Card</h1>;
+export default function Card({ emoji, cardColor }) {
+  const [faceDown, setFaceDown] = useState(true);
+
+  return (
+    <div className="Card">
+      {faceDown === true ? (
+        <div className="down-card" style={{ backgroundColor: cardColor }}>
+          <div className="emoji-frame">{emoji}</div>
+        </div>
+      ) : (
+        "face up"
+      )}
+    </div>
+  );
 }
